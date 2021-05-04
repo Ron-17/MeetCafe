b41f439ed352839e799a22ebc2c0b23b0e8401bc
Ranit <54316475+Ron-17@users.noreply.github.com>
2021-05-04 11:15:59 +0530

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,18 @@
+const express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
+const Employee = mongoose.model('Employee');
+
+router.get('/', (req, res) => {
+    res.render("employee/addOrEdit", {
+        viewTitle: "Insert Employee"
+    });
+});
+
+router.post('/', (req, res) => {
+    if (req.body._id == '')
+        insertRecord(req, res);
+        else
+        updateRecord(req, res);
+});
+
